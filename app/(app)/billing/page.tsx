f32cf19a17fbe604886505a319f565c762984998
Quavo <hunchodotdev@gmail.com>
2024-08-26 20:58:10 +0000

    user profile page

---
+++ app/(app)/billing/page.tsx
@@ -0,0 +1,151 @@
+"use client";
+
+import { CheckIcon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
+const freeFeatures = [
+  "Receive feedback for your project",
+  "Email notifications (once a week)",
+  "Free entry to annual conference",
+];
+
+const paidFeatures = [
+  "Everything in Free Plan",
+  "Email notifications (once every 3 days)",
+  "Free future updates",
+  "24/7 Customer Support",
+];
+
+export default function Billing() {
+  return (
+    <div className="bg-white sm:py-12">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        {/* <div className="mx-auto max-w-2xl sm:text-center">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            Simple no-tricks pricing
+          </h2>
+          <p className="mt-6 text-lg leading-8 text-gray-600">
+            Distinctio et nulla eum soluta et neque labore quibusdam. Saepe et
+            quasi iusto modi velit ut non voluptas in. Explicabo id ut laborum.
+          </p>
+        </div> */}
+        {/* FREE */}
+        <div className="mx-auto max-w-2xl rounded-3xl ring-1 ring-gray-200 lg:mx-0 lg:flex lg:max-w-none">
+          <div className="p-8 sm:p-10 lg:flex-auto">
+            <h3 className="text-2xl font-bold tracking-tight text-gray-900">
+              Free
+            </h3>
+            <p className="mt-6 text-base leading-7 text-gray-600">
+              Free access to basic features. No credit card required.
+            </p>
+            <div className="mt-10 flex items-center gap-x-4">
+              <h4 className="flex-none text-sm font-semibold leading-6 text-indigo-600">
+                What&apos;s included
+              </h4>
+              <div className="h-px flex-auto bg-gray-100" />
+            </div>
+            <ul
+              role="list"
+              className="mt-8 grid grid-cols-1 gap-4 text-sm leading-6 text-gray-600 sm:grid-cols-2 sm:gap-6"
+            >
+              {freeFeatures.map((feature) => (
+                <li key={feature} className="flex gap-x-3">
+                  <CheckIcon
+                    aria-hidden="true"
+                    className="h-6 w-5 flex-none text-indigo-600"
+                  />
+                  {feature}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="-mt-2 p-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0">
+            <div className="rounded-2xl bg-gray-50 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16">
+              <div className="mx-auto max-w-xs px-8">
+                <p className="text-base font-semibold text-gray-600">
+                  Free forever
+                </p>
+                <p className="mt-6 flex items-baseline justify-center gap-x-2">
+                  <span className="text-5xl font-bold tracking-tight text-gray-900">
+                    $0.00
+                  </span>
+                  <span className="text-sm font-semibold leading-6 tracking-wide text-gray-600">
+                    USD
+                  </span>
+                </p>
+                <Button
+                  disabled
+                  className="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Active
+                </Button>
+                <p className="mt-6 text-xs leading-5 text-gray-600">
+                  This is the basic plan available to all users without using a
+                  credit card.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* UPGRADE */}
+
+        <div className="mx-auto mt-16 max-w-2xl rounded-3xl ring-1 ring-gray-200 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none">
+          <div className="p-8 sm:p-10 lg:flex-auto">
+            <h3 className="text-2xl font-bold tracking-tight text-gray-900">
+              Lifetime membership
+            </h3>
+            <p className="mt-6 text-base leading-7 text-gray-600">
+              Pay once, own it forever. Credit card required.
+            </p>
+            <div className="mt-10 flex items-center gap-x-4">
+              <h4 className="flex-none text-sm font-semibold leading-6 text-indigo-600">
+                What&apos;s included
+              </h4>
+              <div className="h-px flex-auto bg-gray-100" />
+            </div>
+            <ul
+              role="list"
+              className="mt-8 grid grid-cols-1 gap-4 text-sm leading-6 text-gray-600 sm:grid-cols-2 sm:gap-6"
+            >
+              {paidFeatures.map((feature) => (
+                <li key={feature} className="flex gap-x-3">
+                  <CheckIcon
+                    aria-hidden="true"
+                    className="h-6 w-5 flex-none text-indigo-600"
+                  />
+                  {feature}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="-mt-2 p-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0">
+            <div className="rounded-2xl bg-gray-50 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16">
+              <div className="mx-auto max-w-xs px-8">
+                <p className="text-base font-semibold text-gray-600">
+                  Pay once, own it forever
+                </p>
+                <p className="mt-6 flex items-baseline justify-center gap-x-2">
+                  <span className="text-5xl font-bold tracking-tight text-gray-900">
+                    $10.00
+                  </span>
+                  <span className="text-sm font-semibold leading-6 tracking-wide text-gray-600">
+                    USD
+                  </span>
+                </p>
+                <Button className="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                  Upgrade
+                </Button>
+                <p className="mt-6 text-xs leading-5 text-gray-600">
+                  Buy OrbitFeed and get access to tons of features and future
+                  updates.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- convex/user.ts
+++ convex/user.ts
@@ -1,13 +1,33 @@
 // THIS IS NOT THE ACTUAL USER MODEL
 // THIS IS USED FOR ACCESSING PARTS OF THE
 // ORIGINAL USER MODEL/TABLE CREATED BY THE AUTH PROVIDER
-import { query } from "./_generated/server"
-import { auth } from "./auth"
+import { v } from "convex/values";
+
+import { query } from "./_generated/server";
+import { auth } from "./auth";
 
 export const viewer = query({
   args: {},
   handler: async (ctx) => {
-    const userId = await auth.getUserId(ctx)
-    return userId !== null ? ctx.db.get(userId) : null
+    const userId = await auth.getUserId(ctx);
+    return userId !== null ? ctx.db.get(userId) : null;
   },
-})
+});
+
+export const account = query({
+  args: {
+    userId: v.id("users"),
+  },
+  handler: async (ctx, args) => {
+    const { userId } = args;
+    const account = await ctx.db
+      .query("authAccounts")
+      .filter((q) => q.eq(q.field("userId"), userId))
+      .unique();
+
+    // remember to paginate the results
+    // return both the account and the length
+
+    return account;
+  },
+});
